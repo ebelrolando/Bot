57296164afa5e48c92563f4e40a2ebe7fcc3872b
ebelrolando <31048589+ebelrolando@users.noreply.github.com>
2020-11-13 03:26:29 +0100

    Update DamienConfig.py

--- DamienConfig.py
+++ DamienConfig.py
@@ -26,4 +26,8 @@ class Messages():
 
       ABOUT_MSG = "©️ https://t.me/DamienSoukara"
       
+      def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+      
       

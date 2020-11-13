fc71603e3cc09436b68c19459ecf172a8a852145
ebelrolando <31048589+ebelrolando@users.noreply.github.com>
2020-11-13 03:40:40 +0100

    Update DamienConfig.py

--- DamienConfig.py
+++ DamienConfig.py
@@ -26,6 +26,38 @@ class Messages():
 
       ABOUT_MSG = "©️ https://t.me/DamienSoukara"
       
-       hola = "Hey! [👋](https://i.imgur.com/Ljhp9Kk.gif) [{}](tg://user?id={}) \n©️ Read /help & /about"
-  
+      
+    def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("BOT_TOKEN", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+      def pizza(update, context): > 
+        if (update.message.reply.text_upper().find("MANZANAS VERDES") > 0 ):
+             update.message.reply_text("Prefiero comer pizza")
+      
+      
       

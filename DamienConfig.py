13d7d953794e28ca6305a1bdb4aa578b1433251d
ebelrolando <31048589+ebelrolando@users.noreply.github.com>
2020-11-13 02:37:16 +0100

    Update DamienConfig.py

--- DamienConfig.py
+++ DamienConfig.py
@@ -7,19 +7,19 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
   else:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "1352432671:AAEzHRx-4F08H2Ja7CjFxnY7MStkB4fQG5Y"
+    APP_ID = "2190647"
+    API_HASH = "c9bec948217c0eae66f8f85e0e48d881"
 
 class Messages():
       HELP_MSG = [
         ".",
 
-        "**Hello 🌍 World**",
+        "**Hola mundo👋**",
         
-        "Another Text 😅",
+        "Otro Texto 😅",
         
-        "**[👨‍💻](https://i.imgur.com/TaOKIkf.gif) Developed By : @AmineSoukara**"
+        "**[👨‍💻](https://i.imgur.com/TaOKIkf.gif) Desarrollado por: @AmineSoukara**"
       ]
 
       X_MSG = "Hey! [👋](https://i.imgur.com/Ljhp9Kk.gif) [{}](tg://user?id={}) \n©️ Read /help & /about"
